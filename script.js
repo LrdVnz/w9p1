257a58437001267715d5dbceb036e1ef81e91353
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-01-14 16:01:15 +0100

    Created fill animation for bookmark

---
+++ script.js
@@ -0,0 +1,18 @@
+
+/* Script di feather Icons */
+feather.replace();
+
+/* Script per creare animazione di riempimento del bookmark */
+
+/* let bookmarks = document.getElementsByClassName("bookmark-icon");
+
+for(single_bookmark of bookmarks){
+  single_bookmark.addEventListener('mouseenter' , (event) => {
+    event.target.setAttribute('fill', 'black');
+  });
+
+  single_bookmark.addEventListener('mouseleave' , (event) => {
+    event.target.setAttribute('fill', 'none');
+  });
+
+} */
